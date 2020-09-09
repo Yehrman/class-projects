f0e770f541f53b86c0ebf86444e1e9bcc4c56f58
Yehrman <yrehrman@gmail.com>
2020-09-09 12:44:21 +0300

    organized and added ado.net project

---
+++ DataAccessProjects/SecuritySystemAdoNet/DataAccess/EmployeeRepository.cs
@@ -0,0 +1,93 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+  public  class EmployeeRepository:IDisposable
+    {
+        private SqlConnection Sqlconn { get; set; }
+
+        public EmployeeRepository()
+        {
+            Sqlconn = CreateConnection();
+        }
+      
+        private IEnumerable<EmployeeModel> ReadEmployee(string predicate,params SqlParameter[] parameters  )
+        {
+            Sqlconn.Open();
+            var cmd = new SqlCommand("select * from Employee e " + predicate, Sqlconn);
+            foreach (var item in parameters)
+            {
+                cmd.Parameters.Add(item);
+            }
+            SqlDataReader reader = cmd.ExecuteReader();
+            while(reader.Read())
+            {
+                yield return new EmployeeModel
+                {
+                    EmployeeId = reader.GetInt32(0),
+                    FirstName = reader.GetString(1),
+                    LastName = reader.GetString(2)
+                };
+            }
+            Sqlconn.Close();
+        }   
+        public IEnumerable<EmployeeModel> ReadEmployeeByPk(int Pk)
+        {
+            return ReadEmployee(
+                "where e.EmployeeId=@Pk",
+                new SqlParameter ("@Pk",Pk)
+                );
+        }
+        public IEnumerable<EmployeeModel> ReadEmployeeByName(string name)
+        {
+            return ReadEmployee("where  e.FirstName like '%'+@Name+ '%' or e.LastName like '%'+ @Name +'%' or @Name='' ",
+                new SqlParameter("@Name", name)
+                );          
+        }
+        public void CreateEmployee(EmployeeModel model)
+        {
+            Sqlconn.Open();
+//            SqlCommand cmd2 = new SqlCommand(@"if exists(select * from Employee e where e.EmployeeId=@Pk)
+//begin
+//update Employee
+//set FirstName = @FName,
+//LastName = @LName where EmployeeId = @Pk
+//end
+//else
+//                begin
+//                insert  Employee(FirstName, LastName)values(@FName, @LName)
+//select scope_identity()
+//end");
+            SqlCommand cmd = new SqlCommand("insert  Employee(FirstName,LastName)values(@FName,@LName)",Sqlconn);
+            cmd.Parameters.AddWithValue("@Fname", model.FirstName);
+            cmd.Parameters.AddWithValue("@LName", model.LastName);
+            // cmd.Parameters.AddWithValue("@Pk", model.EmployeeId);
+            cmd.ExecuteNonQuery();
+            Sqlconn.Close();
+        }
+        public void DeleteEmployee(int Id)
+        {
+            Sqlconn.Open();
+            SqlCommand cmd = new SqlCommand("delete  Employee  Where EmployeeId=@Pk",Sqlconn);
+            cmd.Parameters.AddWithValue("@Pk", Id);
+            cmd.ExecuteNonQuery();
+            Sqlconn.Close();
+        }
+        private SqlConnection CreateConnection()
+        {
+          var Conn=  ConfigurationManager.ConnectionStrings["BuildingSecurityConnection"];
+            return new SqlConnection(Conn.ConnectionString);
+        }
+        public void Dispose()
+        {
+            Sqlconn.Dispose();     
+        }
+    }
+}
---
+++ DataAccessProjects/SecuritySystemAdoNet/SecuritySystemAdoNet/Program.cs
@@ -0,0 +1,96 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecuritySystemAdoNet
+{
+    class Program
+    {
+        public static string Prompt(string val)
+        {
+            Console.WriteLine("Please type in the " + val);
+            Console.Write(">");
+            return Console.ReadLine();
+        }
+        static void FindEmployeeByName()
+        {
+            using (EmployeeRepository repository = new EmployeeRepository())
+            {
+                var name = Prompt("Name");
+                var match = repository.ReadEmployeeByName(name);
+                foreach (var item in match)
+                {
+                    Console.WriteLine($"{item.EmployeeId} {item.FirstName} {item.LastName}");
+                }
+            }
+        }
+        static void FindEmployeeById()
+        {
+            using(EmployeeRepository repository=new EmployeeRepository())
+            {
+                var Id = Prompt("Id");
+                int Pk = int.Parse(Id);
+                var match = repository.ReadEmployeeByPk(Pk);
+                foreach (var item in match)
+                {
+                    Console.WriteLine($"{item.EmployeeId} {item.FirstName} {item.LastName}");
+                }
+            }
+        }
+        static void CreateEmployee()
+        {
+            EmployeeModel model = new EmployeeModel();
+            using (EmployeeRepository repository = new EmployeeRepository())
+            {
+              model.FirstName=  Prompt("first name of the employee");
+               model.LastName= Prompt("last name of employee");
+                repository.CreateEmployee(model);
+            }
+        }
+        static void DeleteEmployee()
+        {
+            EmployeeModel model = new EmployeeModel();
+            using(EmployeeRepository repository=new EmployeeRepository())
+            {
+                var Id = Prompt("Id of the employee to delete");
+                int Pk = int.Parse(Id);
+                repository.DeleteEmployee(Pk);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("To read data type read to add a employee type add to delete a employee press del");
+                string Input = Console.ReadLine();
+                if(Input=="read")
+                {
+                    Console.WriteLine("Type name to search by name or id to serch by id");
+                    string Search = Console.ReadLine();
+                    if(Search=="name")
+                    {
+                        FindEmployeeByName();
+                    }
+                    if(Search=="id")
+                    {
+                        FindEmployeeById();
+                    }
+                }
+                if(Input=="add")
+                {
+                    CreateEmployee();
+                }
+                 if(Input=="del")
+                {
+                    DeleteEmployee();
+                }
+                //Console.WriteLine("Type Id to select by id and ")
+              
+            }
+        }
+    }
+}
